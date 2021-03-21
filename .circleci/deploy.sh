00db1444c6cd9cafef185f36666844193d1aa614
Jacobus Badenhorst <jacobusbad@gmail.com>
2021-03-21 15:21:36 -0600

    fixed deploy.sh

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -4,6 +4,8 @@
 
 cd ~/microservices
 
+git pull origin main
+
 docker-compose -f ./admin/docker-compose-deploy.yml up -d  --build
 docker-compose -f ./main/docker-compose-deploy.yml up -d  --build
 
