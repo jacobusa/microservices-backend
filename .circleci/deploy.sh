236e41976282ecb18060ce0cbc5829f73f862e77
Jacobus Badenhorst <jacobusbad@gmail.com>
2021-03-21 15:55:45 -0600

    testing deploy.sh

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-ssh $USER@$MICROIP "git clone git@github.com:jacobusa/microservices-backend.git && cd ~/microservices-backend/ && docker-compose -f ./admin/docker-compose-deploy.yml up -d  --build && docker-compose -f ./main/docker-compose-deploy.yml up -d  --build"
+# ssh $USER@$MICROIP "git clone git@github.com:jacobusa/microservices-backend.git && cd ~/microservices-backend/ && docker-compose -f ./admin/docker-compose-deploy.yml up -d  --build && docker-compose -f ./main/docker-compose-deploy.yml up -d  --build"
+ssh $USER@$MICROIP "echo $USER && echo $ADMIN_MYSQL_PASSWORD && echo $MAIN_DEBUG"
 # git clone git@github.com:jacobusa/microservices-backend.git
 
 # cd ~/microservices
