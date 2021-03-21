de1db67572221aaf0c80641c39ae350bd78cfa47
Jacobus Badenhorst <jacobusbad@gmail.com>
2021-03-21 15:20:52 -0600

    added CD

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,1 +1,9 @@
-git clone git@github.com:jacobusa/microservices-backend.git
+#!/bin/bash
+
+# git clone git@github.com:jacobusa/microservices-backend.git
+
+cd ~/microservices
+
+docker-compose -f ./admin/docker-compose-deploy.yml up -d  --build
+docker-compose -f ./main/docker-compose-deploy.yml up -d  --build
+
