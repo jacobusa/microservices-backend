8b7de7a4a5812daa51397787f1a9fa37c0c543dc
Jacobus Badenhorst <jacobusbad@gmail.com>
2021-03-23 12:16:46 -0600

    debugging producer

--- main/producer.py
+++ main/producer.py
@@ -12,6 +12,9 @@ channel = connection.channel()
 
 def publish(method, body):
     properties = pika.BasicProperties(method)
+    print(method.values)
+    print(body.values)
+    print(properties.values)
     channel.basic_publish(
         exchange="", routing_key="admin", body=json.dumps(body), properties=properties
     )
